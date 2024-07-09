1de5e7b9e1a15eb556ee6d8b5d2ee794d68339e0
Ishu Kumar Pathak <pishu1766@gmail.com>
2024-07-09 20:39:11 +0530

    edit page   bug

--- graphql/queries/queries.ts
+++ graphql/queries/queries.ts
@@ -1,26 +1,27 @@
 import { gql } from "@apollo/client";
 
 export const GET_CHATBOT_BY_ID = gql`
-query GetChatbotById($id: Int!) {
-  chatbots(where: { id: { _eq: $id } }) {
+
+    query getChatbotById($id:Int){
+    chatbots(id: $id){
     id
     name
     created_at
-    chatbot_characteristics {
-      id
-      content
-      created_at
+    chatbot_characteristics{
+    id
+    content
+    created_at
     }
-    chat_sessions {
-      id
-      created_at
-      guest_id
-      messages {
-        id
-        content
-        created_at
-      }
+    chat_sessions{
+    id
+    created_at
+    guest_id
+    messages{
+    id
+    content
+    created_at
     }
-  }
+    }
+    }
 }
-`;
+        `;
